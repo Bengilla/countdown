5dbef32bb71bba01ca84d24a21be3fc9d5ddc087
Bengilla <bengilla@outlook.com>
2022-02-05 12:26:46 +0800

    final commit

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,54 @@
 import { useEffect, useState } from 'react'
 import NewClock from './newclock'
+import clientPromise from '../lib/mongodb';
 
-export default function Home() {
+
+export default function Home({ countdowns }) {
 
   // Main Clock
   const monthDict = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const [currentYear, setCurrentYears] = useState(0);
   const [currentMonth, setCurrentMonth] = useState(0);
   const [currentDay, setCurrentDays] = useState(0);
-  const [currentHour, setCurrentHours] = useState(0);
-  const [currentMinute, setCurrentMinutes] = useState(0);
-  const [currentSecond, setCurrentSeconds] = useState(0);
   
   // Interval 
   useEffect(() => {
-    const mainInterval = setInterval(() => {
       const now = new Date();
       const date = {
       'year': now.getFullYear(),
       'month': now.getMonth(),
       'day': now.getDate(),
-      'hour': now.getHours(),
-      'min': now.getMinutes(),
-      'sec': now.getSeconds()
       }
       setCurrentYears(date.year);
       setCurrentMonth(date.month);
       setCurrentDays(date.day);
-      setCurrentHours(date.hour.toLocaleString(date.hour, { minimumIntegerDigits: 2 }));
-      setCurrentMinutes(date.min.toLocaleString(date.min, { minimumIntegerDigits: 2 }));
-      setCurrentSeconds(date.sec.toLocaleString(date.sec, { minimumIntegerDigits: 2 }));
-    }, 1000)
-    return () => clearInterval(mainInterval)
-  })
-
-  // Info from DB
-  const title = 'Bengilla Birthday'
-  const day = '15'
-  const month = '8'
-  const year = '2022'
-  const time = '09:00'
-  const date = monthDict[month - 1] + ' ' + day + ', ' + year + ' ' + time 
+  });
 
   return (
     <div className={'container'}>
+
+      <div className='drop'>hello</div>
+
       <div className='mainTime'>
         <h2>{currentDay}, {monthDict[currentMonth]} {currentYear}</h2>
-        <h3>{currentHour} : {currentMinute} : {currentSecond}</h3>
       </div>
-      <NewClock title={title} newDate={date} />
-      <NewClock title='Zuo Yan Birthday' newDate='May, 11 2022 09:00' />
-      <NewClock title='2023 Chinese New Year' newDate='January, 22 2023 09:00' />
+      {countdowns.map(count => (
+        <NewClock title={count.title} newDate={count.month + ', ' + count.day + ' ' + count.year + ' ' + count.time} />
+      ))}
+
     </div>
   )
 }
+
+export async function getStaticProps(context) {
+    const client = await clientPromise
+    const db = client.db('countdownDB');
+
+    const data = await db.collection('countdowns').find({}).toArray() // .find({具体需要的数据比如，年份})
+    const countdowns = JSON.parse(JSON.stringify(data))
+
+    return {
+      props: { countdowns },
+    }
+  }
+
--- pages/newclock.js
+++ pages/newclock.js
@@ -8,41 +8,41 @@ export default function Newclock(props) {
   const [remainMinute, setRemainMinutes] = useState(0);
   const [remainSecond, setRemainSeconds] = useState(0);
 
-  const title = props.title
-
   useEffect(() => {
-    const now = new Date();
-    const target = new Date(props.newDate);
-    const diff = target.getTime() - now.getTime()
+    const Interval = setInterval(() => {
+      const now = new Date();
+      const target = new Date(props.newDate);
+      const diff = target.getTime() - now.getTime()
 
-    // Begin Count Down
-    const d = Math.floor(diff / (1000 * 60 * 60 * 24));
-    setRemainDays(d);
+      // Begin Count Down
+      const d = Math.floor(diff / (1000 * 60 * 60 * 24));
+      setRemainDays(d);
 
-    const h = Math.floor(
-      (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    setRemainHours(h.toLocaleString(h, { minimumIntegerDigits: 2 }));
+      const h = Math.floor(
+        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setRemainHours(h.toLocaleString(h, { minimumIntegerDigits: 2 }));
 
-    const m = Math.floor(
-      (diff % (1000 * 60 * 60)) / (1000 * 60)
-    );
-    setRemainMinutes(m.toLocaleString(m, { minimumIntegerDigits: 2 }));
+      const m = Math.floor(
+        (diff % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      setRemainMinutes(m.toLocaleString(m, { minimumIntegerDigits: 2 }));
 
-    const s = Math.floor(
-      (diff % (1000 * 60)) / 1000
-    );
-    setRemainSeconds(s.toLocaleString(s, { minimumIntegerDigits: 2 }));
-
-  })
+      const s = Math.floor(
+        (diff % (1000 * 60)) / 1000
+      );
+      setRemainSeconds(s.toLocaleString(s, { minimumIntegerDigits: 2 }));
+    }, 1000)
+    return () => clearInterval(Interval)
+  }, [props]);
 
     return (
         <>
         {/* List Begin */}
-        {remainDay <= 0 && remainHour <= 0 && remainMinute <= 0 && remainSecond <= 0 ? <h4 className='box boxComplete'>{title}</h4> :
+        {remainDay <= 0 && remainHour <= 0 && remainMinute <= 0 && remainSecond <= 0 ? <h4 className='box boxComplete'>{props.title}</h4> :
             <div className={remainDay < 1 ? 'box boxOneDay' : 'box boxBegin'}>
             <center>
-                <h4>{title}</h4>
+                <h4>{props.title}</h4>
                 <span><span className="fontRed">Target: </span>{props.newDate}</span><br />
                 <span><span className="fontRed">Left: </span>{remainDay} {remainDay > 1 ? "days" : "day"} ({remainHour}:{remainMinute}:{remainSecond})</span>
             </center>
@@ -50,5 +50,4 @@ export default function Newclock(props) {
         {/* List End */}
         </>
     )
-
 }
